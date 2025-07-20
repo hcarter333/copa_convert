e293dab37e3b463ed58396e99b15739d31dd482b
antigravkids <hcarter333@gmail.com>
2025-07-20 21:59:45 +0000

    add args and fixes output

--- utils/blogmonth.sh
+++ utils/blogmonth.sh
@@ -1,4 +1,6 @@
-python ../utils/blog_arch.py -b https://soldersmoke.blogspot.com/ 2008-05-01T00:00:00Z 2008-05-31T00:00:00Z
+python ../utils/blog_arch.py -b https://soldersmoke.blogspot.com/ $1 $2 -o soldertest 
+git config pull.rebase true
+git pull
 git add soldertest
 git commit -m"more posts"
 git push origin main
